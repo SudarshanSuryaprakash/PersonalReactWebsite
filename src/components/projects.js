6b533acf01cc19d40e7dc13e0acefc596a35d305
Sudarshan Suryaprakash <sudarshan@Sudarshans-MacBook-Air.local>
2020-11-16 11:22:49 +0000

    def

--- src/components/projects.js
+++ src/components/projects.js
@@ -18,8 +18,8 @@ class Projects extends Component {
             title='ProfConnector'
             text=' A Full scale Social Media Web App that allows Professors from
               Brunel University to have a profile and communicate with each
-              other. Uses MongoDB, Express, React and Node. Redux for state
-              management.'
+              other. Uses MongoDB, Express, React and Node. Protects routes with JSON Web Tokens. Extensive backend testing with Postman.
+              Redux is used for state management. Redux is functionality and state management is tested with Redux Dev Tools for Chrome.'
             link='https://ancient-springs-06742.herokuapp.com'
             button_text='Live Demo'
             pic='url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
@@ -28,7 +28,9 @@ class Projects extends Component {
           <ProjectCard
             title='E-Commerce Web App'
             text='Full scale E-Commerce web App rich with features. Built with the
-              same technologies. Ready to be deployed - Could find a client and sell.'
+              MERN stack. Ready to be deployed - Could find a client and sell. 
+              Key features include: Search Product (SP)| SP based on category | New Arrivals | Best Sellers | Product Image | Product Information
+              | Product in stock/Out of stock | View Product | Related Products | Full cart Functionalities | User and Admin Dashboards/Routes | Advanced SP based on Price range and Category'
             link='https://www.linkedin.com/posts/sudarshan-suryaprakash-b5ab1b190_mern-react-javascript-activity-6683104988476002304-H6O8'
             button_text='Video Demo'
             pic='url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
@@ -36,7 +38,21 @@ class Projects extends Component {
           <div className='breaker'></div>
           <ProjectCard
             title='Streaming App'
-            text='Video and Audio Streaming app built for an interview.'
+            text="During the pandemic, there has been an increasing demand for online education - this increases demand for streaming/video-calling apps such as zoom.
+
+I developed my own streaming app that allows one to share their screen, voice to any number of people who tune in.
+|||
+Technologies used-
+Front end: React
+State Management: Redux
+Back End Servers: api - JSON server, rtmp - node-media-server
+Recording Application - OBS
+|||
+Features - 
+Google Authentication
+Watch streams if not logged in
+Create streams if you're logged in.
+Edit/Delete streams if you created the stream"
             link='https://www.linkedin.com/posts/sudarshan-suryaprakash-b5ab1b190_hi-everyone-during-the-pandemic-there-activity-6724369565226213376-d27j'
             button_text='Video Demo'
             pic='url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
@@ -50,7 +66,9 @@ class Projects extends Component {
 
           <ProjectCard
             title='COVID 19 Tracker'
-            text='React app to track covid 19 using an external API'
+            text="Uses an external API to obtain current data on the number of affected, dead and cured people due to the COVID-19. The app uses React, Material UI and Chart.js
+            Corona Virus is, unfortunately, spreading through the entire world.
+            As a developer, I decided to help by visualizing the data and spreading awareness. The data is fetched from an external API. This project/application is created using the most modern JavaScript syntax (Async/Await, arrow functions, Hooks). "
             link='https://covid19trackersud.herokuapp.com'
             button_text='Live Demo'
             pic='url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
@@ -58,7 +76,9 @@ class Projects extends Component {
           <div className='breaker'></div>
           <ProjectCard
             title='A* Path Find Visualizer'
-            text='React app that visualizes the A* path finding algorithm'
+            text='A* is a graph traversal and path search algorithm, which is often used in many fields of computer science due to its completeness, optimality, and optimal efficiency.
+            In this Project, I find the optimal path from the top left of the screen to the bottom right - past randomly generated obstacles. React was used to create the front end and modern CSS techniques (keyframes and animations) and Sass were used to animate the tiles.
+            '
             link='https://pathfind-viz.vercel.app'
             button_text='Live Demo'
             pic='url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'
